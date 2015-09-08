4bc091dbfed24f29271eab5e725cafda6231dcf3
Retesh <retesh.bajaj@gmail.com>
2015-09-08 11:39:02 +0100

    got access token to work

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,11 +1,13 @@
-githubUserSearch.controller('GitUserSearchController', [function() {
+githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
 
   var self = this;
-  var searchResource = $resource('https://api.github.com/search/users')
+  var searchResource = $resource('https://api.github.com/search/users');
 
   self.doSearch = function() {
+    console.log(token)
     self.searchResult = searchResource.get(
-    { q: self.searchTerm }
-  };
-
+      { q: self.searchTerm,
+        access_token: token }
+    );
+  }
 }]);
